590c67c0b76b3b6f7ff315ca9eb7fa0732603ac8
Alan <alanvinicius.ce@gmail.com>
2020-03-25 20:33:44 -0300

    backend finished.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express'); // importing express framework
 const routes = require('./routes') // importing the routes
+const cors = require('cors')
 
 const app = express(); // initialize the app with express
 
+app.use(cors());
 app.use(express.json()); // Express now are waiting requests with JSON format
 app.use(routes);
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response)=>{
-    const body = request.body;
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-    console.log(body);
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
-    return response.json({
-        "name": "Alan Ensina",
-        "url": "github.com/alanensina"
-    });
-});
+routes.get('/profile', ProfileController.index);
+
+routes.post('/sessions', SessionController.create);
+
 
 module.exports = routes;
